a3150bd8b028015f2cdf4efe7e7d94b2b900cbdb
Paulo Meneghini <paulomeneghini@MacBook-Air-de-Paulo.local>
2023-03-16 20:06:12 -0300

    Iniciando as comparações

--- app.py
+++ app.py
@@ -10,12 +10,24 @@ while True:
 
     if option == 1:
         print("Insira os parametros abaixo")
-        particulaInalavel = float(input(" partículas inaláveis (MP10): "))
-        particulaInalavelFina = float(input(" Partículas inaláveis finas (MP2,5):  "))
-        ozonio = float(input(" Ozônio (O3): "))
-        monoxidoDeCarbono = float(input(" monóxido de carbono (CO):  "))
-        dioxidoDeNitrogenio = float(input(" Dióxido de nitrogênio (NO2): "))
-        dioxidoDeEnxofre = float(input(" Dióxido de enxofre (SO2):  "))
+        MP10 = float(input(" Partículas inaláveis (MP10): "))
+        MP25 = float(input(" Partículas inaláveis finas (MP2,5):  "))
+        #FMC = float(input(" Ozônio (O3): "))
+        #O3 = float(input(" monóxido de carbono (CO):  "))
+        #CO = float(input(" Dióxido de nitrogênio (NO2): "))
+        #NO2 = float(input(" Dióxido de enxofre (SO2):  "))
+        #SO2 = float(input("Dióxido de enxofre (SO2): "))
+
+        if MP10 <= 50 or MP25 <= 25: #or O3 <= 100 or CO <= 9 or NO2 <= 200 or SO2 <= 20:
+            print("\nA qualidade do ar é N1 - Boa\n")
+        elif MP10 in range(51, 101) or MP25 in range(26, 51): #or O3 > 100 or CO > 9 or NO2 > 200 or SO2 > 20:
+            print("\nA qualidade do ar é N2 - Moderada\n")
+        elif MP10 in range(101, 151): #or MP25 <= 75 or O3 <= 160 or CO <= 13 or NO2 <= 320 or SO2 <= 365:
+            print("\nA qualidade do ar é N3 - Ruim\n")
+        elif MP10 in range(151, 251): #or MP25 <= 125 or O3 <= 200 or CO <= 15 or NO2 <= 1130 or SO2 <= 800:
+            print("\nA qualidade do ar é N4 - Muito Ruim\n")
+        elif MP10 > 250: #or MP25 > 125 or O3 > 200 or CO > 15 or NO2 > 1130 or SO2 > 800:
+            print("\nA qualidade do ar é N5 - Péssima\n")
     
     elif option == 4:
         break
