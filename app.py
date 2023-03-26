afd7e14f9cb4e84360a5e719bddbefe19226f478
Samuel Fagundes Cesca <samuel1cesca@gmail.com>
2023-03-26 15:48:58 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -8,33 +8,6 @@ print("Air quality.")
 
 #     option = int(input("Digite a opção desejada: "))
 
-
-qualidadeBoa = False
-
-
-def qualidadeN1():
-    if MP10 in range(0, 50) or MP25 in range(0, 25) or O3 in range(0, 100) or CO in range(0, 9) or NO2 in range(0, 200) or SO2 in range(0, 20):
-        qualidadeBoa = True
-    return qualidadeBoa
-    # print("\nA qualidade do ar é N1 - Boa\n")
-
-
-def qualidadeN2():
-    print("\nA qualidade do ar é N2 - Moderada\n")
-
-
-def qualidadeN3():
-    print("\nA qualidade do ar é N3 - Ruim\n")
-
-
-def qualidadeN4():
-    print("\nA qualidade do ar é N4 - Muito Ruim\n")
-
-
-def qualidadeN5():
-    print("\nA qualidade do ar é N5 - Péssima\n")
-
-
 qualityN1 = []
 qualityN2 = []
 qualityN3 = []
@@ -43,79 +16,79 @@ qualityN5 = []
 
 
 def verifyMP10(MP10):
-    if MP10 in range(0, 50):
+    if MP10 in range(0, 51):
         # só entra se for valor valido
         qualityN1.append(MP10)
-    elif MP10 in range(51, 100):
+    elif MP10 in range(51, 101):
         qualityN2.append(MP10)
-    elif MP10 in range(101, 150):
+    elif MP10 in range(100, 151):
         qualityN3.append(MP10)
-    elif MP10 in range(151, 250):
+    elif MP10 in range(151, 251):
         qualityN4.append(MP10)
     elif MP10 > 250:
         qualityN5.append(MP10)
 
 
 def verifyMP25(MP25):
-    if MP25 in range(0, 25):
+    if MP25 in range(0, 26):
         qualityN1.append(MP25)
-    elif MP25 in range(26, 50):
+    elif MP25 in range(26, 51):
         qualityN2.append(MP25)
-    elif MP25 in range(51, 75):
+    elif MP25 in range(51, 76):
         qualityN3.append(MP25)
-    elif MP25 in range(76, 125):
+    elif MP25 in range(76, 126):
         qualityN4.append(MP25)
     elif MP25 > 125:
         qualityN5.append(MP25)
 
 
 def verifyO3(O3):
-    if O3 in range(0, 100):
+    if O3 in range(0, 101):
         qualityN1.append(O3)
-    elif O3 in range(101, 130):
+    elif O3 in range(101, 131):
         qualityN2.append(O3)
-    elif O3 in range(131, 160):
+    elif O3 in range(131, 161):
         qualityN3.append(O3)
-    elif O3 in range(161, 200):
+    elif O3 in range(161, 201):
         qualityN4.append(O3)
     elif O3 > 200:
         qualityN5.append(O3)
 
 
 def verifyCO(CO):
-    if CO in range(0, 9):
+    if CO in range(0, 10):
         qualityN1.append(CO)
-    elif CO in range(10, 11):
+    elif CO in range(10, 12):
         qualityN2.append(CO)
-    elif CO in range(12, 13):
+    elif CO in range(12, 14):
         qualityN3.append(CO)
-    elif CO in range(14, 15):
+    elif CO in range(14, 16):
         qualityN4.append(CO)
     elif CO > 15:
-        qualityN4.append(CO)
+        qualityN5.append(CO)
 
 
 def verifyNO2(NO2):
-    if NO2 in range(0, 200):
+    if NO2 in range(0, 201):
         qualityN1.append(NO2)
-    elif NO2 in range(201, 240):
+    elif NO2 in range(201, 241):
         qualityN2.append(NO2)
-    elif NO2 in range(241, 320):
+    elif NO2 in range(241, 321):
         qualityN3.append(NO2)
-    elif NO2 in range(321, 1130):
+    elif NO2 in range(321, 1131):
         qualityN4.append(NO2)
     elif NO2 > 1130:
         qualityN5.append(NO2)
 
 
 def verifySO2(SO2):
-    if SO2 in range(0, 20):
+    if SO2 in range(0, 21):
         qualityN1.append(SO2)
-    elif SO2 in range(21, 40):
+    elif SO2 in range(21, 41):
         qualityN2.append(SO2)
-    elif SO2 in range(41, 365):
+    elif SO2 in range(41, 366):
         qualityN3.append(SO2)
-    elif SO2 in range(366, 800):
+    elif SO2 in range(366, 801):
         qualityN4.append(SO2)
     elif SO2 > 800:
         qualityN5.append(SO2)
@@ -150,8 +123,22 @@ verifyCO(CO)
 verifyNO2(NO2)
 verifySO2(SO2)
 print(qualityN1)
+print(qualityN2)
+print(qualityN3)
+print(qualityN4)
+print(qualityN5)
 
-if (len(qualityN1)) == 6:
+if (len(qualityN5)) >= 1:
+    print("\nA qualidade do ar é N5 - Péssima\n")
+elif (len(qualityN4) >= 1):
+    print("\nA qualidade do ar é N4 - Muito Ruim")
+elif (len(qualityN3) >= 1):
+    print("\nA qualidade do ar é N3 - Ruim")
+elif (len(qualityN2) >= 1):
+    print("\nA qualidade do ar é N2 - Moderado")
+else:
     print("\nA qualidade do ar é N1 - Boa\n")
+    
+
 
 # chacon@puccampinas.edu.br
