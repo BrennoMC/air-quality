7f9cb440ad430d44a1f245ee5e36445367865b73
Samuel Cesca <samuel1cesca@gmail.com>
2023-03-27 14:57:49 -0300

    Adicionando dic

--- app.py
+++ app.py
@@ -1,100 +1,83 @@
-print("Air quality.")
-# print("MENU")
-# while True:
-#     print("1 - Inserção")
-#     print("2 - Consulta")
-#     print("3 - Exclusão")
-#     print("4 - Sair")
 
-#     option = int(input("Digite a opção desejada: "))
+MP10_quality = {
+    'Boa': list(range(0, 51)),
+    'Moderada': list(range(51, 101)), 
+    'Ruim': list(range(101, 151)), 
+    'Muito Ruim': list(range(151, 251))    
+}
 
-qualityN1 = []
-qualityN2 = []
-qualityN3 = []
-qualityN4 = []
-qualityN5 = []
+MP25_quality = {
+    'Boa': list(range(0, 26)),
+    'Moderada': list(range(26, 51)), 
+    'Ruim': list(range(51, 76)), 
+    'Muito Ruim': list(range(76, 126))    
+}
 
+O3_quality = {
+    'Boa': list(range(0, 101)),
+    'Moderada': list(range(101, 131)), 
+    'Ruim': list(range(131, 161)), 
+    'Muito Ruim': list(range(161, 201))    
+}
 
-def verifyMP10(MP10):
-    if MP10 in range(0, 51):
-        # só entra se for valor valido
-        qualityN1.append(MP10)
-    elif MP10 in range(51, 101):
-        qualityN2.append(MP10)
-    elif MP10 in range(100, 151):
-        qualityN3.append(MP10)
-    elif MP10 in range(151, 251):
-        qualityN4.append(MP10)
-    elif MP10 > 250:
-        qualityN5.append(MP10)
+CO_quality = {
+    'Boa': list(range(0, 10)),
+    'Moderada': list(range(10, 12)), 
+    'Ruim': list(range(12, 14)), 
+    'Muito Ruim': list(range(14, 16))    
+}
 
+NO2_quality = {
+    'Boa': list(range(0, 201)),
+    'Moderada': list(range(201, 241)), 
+    'Ruim': list(range(241, 321)), 
+    'Muito Ruim': list(range(321, 1131))    
+}
 
-def verifyMP25(MP25):
-    if MP25 in range(0, 26):
-        qualityN1.append(MP25)
-    elif MP25 in range(26, 51):
-        qualityN2.append(MP25)
-    elif MP25 in range(51, 76):
-        qualityN3.append(MP25)
-    elif MP25 in range(76, 126):
-        qualityN4.append(MP25)
-    elif MP25 > 125:
-        qualityN5.append(MP25)
+SO2_quality = {
+    'Boa': list(range(0, 21)),
+    'Moderada': list(range(21, 41)), 
+    'Ruim': list(range(41, 366)), 
+    'Muito Ruim': list(range(366, 801))    
+}
 
+quality = []
 
-def verifyO3(O3):
-    if O3 in range(0, 101):
-        qualityN1.append(O3)
-    elif O3 in range(101, 131):
-        qualityN2.append(O3)
-    elif O3 in range(131, 161):
-        qualityN3.append(O3)
-    elif O3 in range(161, 201):
-        qualityN4.append(O3)
-    elif O3 > 200:
-        qualityN5.append(O3)
+def verify(poluente, qualidade):
+    pessimo = True
 
+    for k, v in qualidade.items():
+        for v in v:
+            if v == poluente:
+                quality.append(f'{k}')
+                pessimo = False
+        
+    if pessimo == True:
+        quality.append('Péssima')
 
-def verifyCO(CO):
-    if CO in range(0, 10):
-        qualityN1.append(CO)
-    elif CO in range(10, 12):
-        qualityN2.append(CO)
-    elif CO in range(12, 14):
-        qualityN3.append(CO)
-    elif CO in range(14, 16):
-        qualityN4.append(CO)
-    elif CO > 15:
-        qualityN5.append(CO)
+def prioridade(qualidade):
+    prioridade = 0
+    indice = 0
 
+    for qualidade in qualidade:
+        if qualidade == 'Boa' and prioridade == 0:
+            prioridade = 0
+            indice = qualidade
+        elif qualidade == 'Moderada' and prioridade <= 1:
+            prioridade = 1
+            indice = qualidade
+        elif qualidade == 'Ruim' and prioridade <= 2:
+            prioridade = 2
+            indice = qualidade
+        elif qualidade == 'Muito Ruim' and prioridade <= 3:
+            prioridade = 3
+            indice = qualidade
+        elif qualidade == 'Péssima' and prioridade <= 4:
+            prioridade = 4
+            indice = qualidade
 
-def verifyNO2(NO2):
-    if NO2 in range(0, 201):
-        qualityN1.append(NO2)
-    elif NO2 in range(201, 241):
-        qualityN2.append(NO2)
-    elif NO2 in range(241, 321):
-        qualityN3.append(NO2)
-    elif NO2 in range(321, 1131):
-        qualityN4.append(NO2)
-    elif NO2 > 1130:
-        qualityN5.append(NO2)
+    return indice
 
-
-def verifySO2(SO2):
-    if SO2 in range(0, 21):
-        qualityN1.append(SO2)
-    elif SO2 in range(21, 41):
-        qualityN2.append(SO2)
-    elif SO2 in range(41, 366):
-        qualityN3.append(SO2)
-    elif SO2 in range(366, 801):
-        qualityN4.append(SO2)
-    elif SO2 > 800:
-        qualityN5.append(SO2)
-
-
-valoresDigitados = []
 print("Insira os parametros abaixo")
 MP10 = float(input(" Partículas inaláveis (MP10): "))
 MP25 = float(input(" Partículas inaláveis finas (MP2,5): "))
@@ -103,42 +86,14 @@ CO = float(input(" Monóxido de carbono (CO2): "))
 NO2 = float(input(" Dióxido de nitrogênio (NO2): "))
 SO2 = float(input(" Dióxido de enxofre (SO2): "))
 
-'''
-if indice >= 0 and indice <= 40:
-    print("\nA qualidade do ar é N1 - Boa\n")
-elif indice >= 41 and indice <= 80:
-    print("\nA qualidade do ar é N2 - Moderada\n")
-elif indice >= 81 and indice <= 120:
-    print("\nA qualidade do ar é N3 - Ruim\n")
-elif indice >= 121 and indice <= 200:
-    print("\nA qualidade do ar é N4 - Muito Ruim\n")
-else:
-    print("\nA qualidade do ar é N5 - Péssima\n")
-'''
+verify(MP10, MP10_quality)
+verify(MP25, MP25_quality)
+verify(O3, O3_quality)
+verify(CO, CO_quality)
+verify(NO2, NO2_quality)
+verify(SO2, SO2_quality)
 
-verifyMP10(MP10)
-verifyMP25(MP25)
-verifyO3(O3)
-verifyCO(CO)
-verifyNO2(NO2)
-verifySO2(SO2)
-print(qualityN1)
-print(qualityN2)
-print(qualityN3)
-print(qualityN4)
-print(qualityN5)
+print(quality)
+print(prioridade(quality))
 
-if (len(qualityN5)) >= 1:
-    print("\nA qualidade do ar é N5 - Péssima\n")
-elif (len(qualityN4) >= 1):
-    print("\nA qualidade do ar é N4 - Muito Ruim")
-elif (len(qualityN3) >= 1):
-    print("\nA qualidade do ar é N3 - Ruim")
-elif (len(qualityN2) >= 1):
-    print("\nA qualidade do ar é N2 - Moderado")
-else:
-    print("\nA qualidade do ar é N1 - Boa\n")
-    
-
-
-# chacon@puccampinas.edu.br
+        
