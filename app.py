c073d2d468174fb9ef5ea595241b96605edfdb26
Samuel Fagundes Cesca <samuel1cesca@gmail.com>
2023-05-02 20:32:48 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,26 @@
 import conn
 import mysql.connector
 from mysql.connector import Error
+import tkinter as tk
+
+root = tk.Tk()
+root.title('Air Quality')
+
+window_width = 800
+
+window_height = 600
+
+screen_width = root.winfo_screenwidth()
+
+screen_height = root.winfo_screenheight()
+
+center_x = int(screen_width/2 - window_width / 2)
+
+center_y = int(screen_height/2 - window_height / 2)
+
+root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
+
+root.mainloop()
 
 MP10_quality = {
     'Boa': list(range(0, 51)),
